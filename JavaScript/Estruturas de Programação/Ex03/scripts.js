597abd5bcb74ebacb4be76acbe66da8fa4749ab6
Diego Miguel <diegomiguel.201@gmail.com>
2020-03-31 15:18:19 -0300

    add old files

---
+++ JavaScript/Estruturas de Programação/Ex03/scripts.js
@@ -0,0 +1,12 @@
+//Uso da biblioteca Math.pow para calcular potencias
+
+
+console.log("2 elevado a 2 é: " + Math.pow(2,2));
+console.log("3 elevado a 2 é: " + Math.pow(3,2));
+console.log("18 elevado a 2 é: " + Math.pow(18,2));
+
+let num1 = 6;
+let num2 = 8;
+
+console.log(num1 + " " + "elevado a " + num2 + " é: " + Math.pow(num1,num2));
+console.log(num1 + " " + "elevado a 3 é: " + Math.pow(num1,3));
---
+++ JavaScript/Estruturas de Programação/Ex04/scripts.js
@@ -0,0 +1,10 @@
+//Uso de Estrutura if else
+
+let velocidade = 90;
+
+if (velocidade <= 80){
+    console.log("Velocidade dentro da permitida");
+}
+else{
+    console.log("Velocidade fora do permitido!!");
+}

4bbbad6a2eb4a8f74dc4cdd6e52fc633f357c8e1
Diego Miguel <diegomiguel.201@gmail.com>
2020-04-13 11:52:59 -0300

    add first class and methods exercises

---
+++ Java/Class and Methods Exercises/src/entities/Rectangle.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Rectangle {
+	public double width;
+	public double height;
+	
+	public double Area () {
+		return width * height;
+			
+	}
+	
+	public double Perimeter() {
+		return 2 * (width + height);
+	}
+	
+	public double Diagonal() {
+		double diag;
+		
+		diag = Math.pow(width, 2) + Math.pow(height, 2);
+		
+		return Math.sqrt(diag);
+		
+	}
+	
+}

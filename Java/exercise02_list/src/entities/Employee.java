055e5c88a1fe1914346b2ad6e1e9dda134e74cbb
Diego Miguel <diegomiguel.201@gmail.com>
2020-04-28 19:43:28 -0300

    add list exercise

---
+++ Java/exercise02_list/src/entities/Employee.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	//Methods
+	
+	public void increaseSalary(Double percentage) {
+		this.salary = salary * percentage + salary;
+	}
+	
+	public String ToString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+}
+	
+}
